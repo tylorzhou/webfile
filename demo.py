f5d42ac233d552fa9585cd545c518eb89c873ddc
tylorzhou <tylorzhou0924@gmail.com>
2016-01-29 00:25:07 -0800

    little demo finish

--- demo.py
+++ demo.py
@@ -42,6 +42,46 @@ def initdb_command():
     init_db()
     print ('initialized the database')
     
-    
+@app.route('/add', methods = ['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?, ?)',
+				[request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+	
+	
+@app.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries = entries)
 
-        
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	print('coming', request.method)
+	if request.method == 'POST':
+		print('post', request.form['username'], ' ' , app.config['USERNAME'])
+		print('post', request.form['password'], ' ' , app.config['PASSWORD'])
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'invalid account'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+		print(error)
+	return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+	
